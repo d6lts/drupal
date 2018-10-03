933cefbdfbc7d27c55a8d2389aff0502fb2545a3
Viktor Szépe <viktor@szepe.net>
2018-10-03 12:43:06 +0000

    Start with phpstan

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+define('MAINTENANCE_MODE', 'install');
+
+function install_no_profile_error() {}
+function page_cache_fastpath() {}
+function recode_string() {}
+function drupal_mail_wrapper($one) {}
+function custom_url_rewrite_inbound($one, $two, $three) {}
